98df995114fb3b29664fd61ac06c69fe801b669c
Prabhjeet Singh <prabhjeet.kc2016@gmail.com>
2025-08-17 12:46:23 +0000

    add Paper No. ..........................................................

--- src/app/(main)/bill-form/[evaluatorId]/page.tsx
+++ src/app/(main)/bill-form/[evaluatorId]/page.tsx
@@ -218,6 +218,10 @@ export default function BillViewPage() {
                             <span className="font-mono">{billDetails.ifscCode}</span>
                         </div>
                     </div>
+                     <div className="pt-2 flex justify-between">
+                        <span>Paper No. ..................................................................................................................</span>
+                        <span className="ml-4">Duration of Paper ..................</span>
+                    </div>
                 </div>
                 
                 <div className="pt-2">
