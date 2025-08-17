52b420e72653b932601bd2546de944c36110da79
Prabhjeet Singh <prabhjeet.kc2016@gmail.com>
2025-08-17 12:32:12 +0000

    allign Date of visits text at the top in the boxicell in view forms and

--- src/app/(main)/bill-form/[evaluatorId]/page.tsx
+++ src/app/(main)/bill-form/[evaluatorId]/page.tsx
@@ -240,7 +240,7 @@ export default function BillViewPage() {
                                 <TableCell className="border print-table"></TableCell>
                             </TableRow>
                              <TableRow>
-                                <TableCell className="font-bold border print-table py-8 align-top" colSpan={2}>
+                                <TableCell className="font-bold border print-table py-1 align-top" colSpan={2}>
                                     Date of Visits: 
                                 </TableCell>
                             </TableRow>
@@ -306,7 +306,7 @@ export default function BillViewPage() {
                             <span>Refreshment (125x &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;) :</span>
                             <span className="text-right">Rs. ______________________</span>
                         </div>
-                         <div className="flex justify-between items-center font-semibold">
+                         <div className="flex justify-between items-center">
                             <span>Net Payable :</span>
                             <span className="text-right">Rs. ______________________</span>
                         </div>
