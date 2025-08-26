7b86b674458b40766f3cde0a803389e8ba00756d
Prabhjeet Singh <prabhjeet.kc2016@gmail.com>
2025-08-26 17:45:35 +0000

    if place mouse cursor on logout button then it ill display red colour

--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -27,7 +27,7 @@ export default function LogoutButton() {
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
-        <Button variant="ghost">
+        <Button variant="ghost" className="hover:bg-destructive hover:text-destructive-foreground">
           <LogOut className="mr-2 h-4 w-4" />
           Logout
         </Button>
